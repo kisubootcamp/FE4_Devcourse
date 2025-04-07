d765c5fd134a7eccdf594388e55e6fab8d947b54
YooJung Kwon <best106yj@naver.com>
2025-04-08 01:02:54 +0900

    250407_TS과제(필수 js문법, ts 기본타입 정리)

---
+++ learn-ts/src/01/index.ts
@@ -0,0 +1,35 @@
+// 트랜스 컴파일(컴파일링)
+// ts -> js
+// 트랜스 컴파일링(트랜스 컴파일을 수행하는 도구)
+// tsc -> 트랜스 컴파일러
+// const str = "Hello World!!!!!!";
+// console.log(str.replaceAll(" ", "")); // es2021
+
+// 타입스크립트
+// 자바스크립트 + 타입 시스템을 추가한 언어
+
+// 자바스크립트 -> 타입스크립트
+// 자바스크립트 -> 문법
+// 타입스크립트 타입 시스템을 배우는 것.
+// 타입스크립트는 결국 값의 타입을 지정해주는 것.
+// 타입스크립트가 왜 어렵다는 생각이 드냐면?
+// 값에 대한 타입을 지정하는 방법이 많음
+
+// string, number, boolean, object, array, symbal, tuple, undefined, aull, any, bigint
+// 정적 타이핑
+
+// 자바스크립트 -> 동적 타입 시스템
+// 타입스크립트 -> 정적 타입 시스템
+{
+  let name: string = "sucoding";
+  let age: number = 20;
+  let bool: boolean = true;
+
+  let obj: object = {}; // o -> 추후 자세히 설명 예정
+  let arr: [] = []; // o -> 추후 자세히 설명 예정
+
+  let sym: symbol = Symbol("a");
+  let und: undefined = undefined; // 작성할 일 없다.
+  let nul: null = null;
+  let big: bigint = 100n;
+}
