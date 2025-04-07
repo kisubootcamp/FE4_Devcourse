8ead634ebb81cc63a46be01541e37c6e033b6544
run3go <qkrwjdtn09@gmail.com>
2025-04-07 17:55:30 +0900

    과제 day1

---
+++ learn-ts/src/01/index.ts
@@ -0,0 +1,21 @@
+// 타입스크립트
+// 자바스크립트 + 타입 시스템을 추가한 언어
+
+// 타입스크립트는 결국 값의 타입을 지정해주는 것
+// 값에 대한 타입을 지정하는 방법이 많음
+
+// string, number, boolean, object, array, symbol, tuple, undefined, null, any, bigint
+{
+  let name: string = "sucoding";
+  let age: number = 20;
+  let bool: boolean = true;
+
+  // o -> 자세히 설명해줌
+  let obj: object = {};
+  let arr: [] = [];
+
+  let sym: symbol = Symbol("a");
+  let und: undefined = undefined;
+  let nul: null = null;
+  let big: bigint = 100n;
+}
