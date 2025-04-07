b0e580cc71b10b2933eaea9ee8016e67aca5cd45
TYss00 <rlaxodus203@naver.com>
2025-04-07 16:17:22 +0900

    tyscript ex

---
+++ learn-ts/src/01/index.ts
@@ -0,0 +1,24 @@
+//npx tsc src/01/index.ts(경로) -> 자바스크립트로 변경해줌
+//npx tsc 경로
+// 타입스크립트
+// 자바스크립트 + 타입시스템을 추가한 언어
+// 타입스크립트는 결국 값의 타입을 지정해주는것
+
+// string,number,boolean,object,array,symbol,tuple,undefined, null, any, bigint
+{
+    let name: string = 'sucoding';
+    let age: number = 20;
+    let bool: boolean = true;
+
+    let MAX_LEVEL: number = 99;
+    // MAX_LEVEL = '9'; // 타입을 숫자로 지정했으므로 에러 체크됨
+
+    // 나중에 설명
+    let obj: object = {};
+    let arr: [] = [];
+
+    let sym: symbol = Symbol('a');
+    let und: undefined = undefined;
+    let nul: null = null;
+    let big: bigint = 100n;
+}
