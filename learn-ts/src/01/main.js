f55e045b6b1888f3d8363c1057c43106d0a68024
kang-min-ryu <dbrkdalswkd@gmail.com>
2025-04-07 16:38:39 +0900

    do the task

---
+++ learn-ts/src/01/main.js
@@ -0,0 +1,102 @@
+// 1. 템플릿 문자열
+// 1.1 백틱(`) 문자열을 정의하는 방법,
+
+const { idText } = require("typescript");
+
+const uname = "sucoding";
+const greet = `Hello, ${uname}`;
+console.log(greet);
+
+// 2. 화살표 함수
+// 2.1 함수를 화살표를 사용해서 정의하는 방법
+// 2.1.1 함수 선언문
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// // 2.1.2 함수 표현식
+// const sum = function sum(a, b) {
+//   return a + b;
+// };
+
+// // 2.1.3 화살표 함수
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// const sum = (a, b) => a + b;
+
+// 3. 구조 분해 할당(비구조화 할당)
+// 3.1 분해해서 할당하겠다.
+
+const [a, ...rest] = ["apple", "banana", "orange"];
+console.log(a);
+console.log(rest);
+
+// 3.2 객체에서의 구조 분해 할당
+const {
+  animalName: aname,
+  animalType,
+  animalAge,
+  animalGender,
+} = {
+  animalName: "곰이",
+  animalType: "고양이",
+  animalAge: 10,
+  animalGender: "male",
+};
+
+console.log(aname);
+console.log(animalType);
+console.log(animalAge);
+console.log(animalGender);
+
+const {
+  id,
+  profile: { name: sname, age, gender },
+} = {
+  id: 1,
+  profile: {
+    name: "곰이",
+    age: 20,
+    gender: "male",
+  },
+};
+console.log(sname, age, gender);
+
+function printInfo({ name, age }) {
+  console.log(`${name}, ${age}`);
+}
+
+printInfo({
+  name: "sucoding",
+  age: 20,
+});
+
+// 4. 계산된 속성
+// 4.1 객체의 키를 변수로 치환
+const key = "name";
+const user = {
+  [key]: "sucoding",
+};
+console.log(key);
+
+// 5. 스프레드 연산자
+// const [red, ...restColor] = ["red", "blue", "green"];
+// console.log(red);
+
+// console.log(restColor);
+
+// 같은 복사처럼 보이는 얇은 복시 -> 방문 목사
+// 깊은 복사 (deep copy)
+// 얇은 복사 (shallow copy)
+// 1 중첩이라는 확신이 있을 때 !!!!!!!!!!!!
+const colors = ["red", "orange", "banana", { special: "pink" }];
+const copyColors = [...colors];
+
+copyColors[0] = "violet";
+copyColors[3].special = "grey";
+
+console.log(colors);
+console.log(copyColors);
