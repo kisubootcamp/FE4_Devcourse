48399f0bf1d96f3c09b25d72b561ff2b17c77151
JINJIYU23 <dhkswksla22@naver.com>
2025-04-08 12:46:36 +0900

    typescript 연습문제

--- learn_TS/src/01/index.ts
+++ learn_TS/src/01/index.ts
@@ -1,17 +1,17 @@
 // 자바스크립트 + 타입 (자바스크립트에 타입 시스템을 추가한 언어)
 // string, number, boolean, object, array, tuple, symbol, undefined, null, any, bigint
 {
-    let name: string = "Jeong";
-    let age : number = 20;
-    let bool : boolean = true;
-    let obj : object = {};
-    let arr : [] = [];
-    let sym : symbol = Symbol("a");
-    let und : undefined = undefined;
-    let nul : null = null;
-    let big : bigint = 100n;
+  let name: string = "Jeong";
+  let age: number = 20;
+  let bool: boolean = true;
+  let obj: object = {};
+  let arr: [] = [];
+  let sym: symbol = Symbol("a");
+  let und: undefined = undefined;
+  let nul: null = null;
+  let big: bigint = 100n;
+  console.log(name);
 }
-
 
 // 자바스크립트 -> 동적 타입 시스템
 // 타입스크립트 -> 정적 타입 시스템
