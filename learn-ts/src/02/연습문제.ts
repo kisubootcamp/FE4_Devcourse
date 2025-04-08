0fdc14c3d2e6e4d9c5a04fd3ae97a1947967edf2
Suji Hwang <soojiya2003@gmail.com>
2025-04-08 12:46:58 +0900

    typescript 기본타입 연습문제 풀이

---
+++ learn-ts/src/02/연습문제.ts
@@ -0,0 +1,40 @@
+{
+  //1. 문자열 변수 선언
+  const str: string = "Hello";
+  console.log(str);
+
+  //2. 숫자 변수 선언
+  const num: number = 42;
+  console.log(num);
+
+  //3. 불리언 변수 선언
+  const bool: boolean = true;
+  console.log(bool);
+
+  //4. 숫자 배열 선언
+  const numArr: number[] = [1, 2, 3];
+  console.log(numArr);
+
+  //5. 문자열 배열 선언
+  const strArr: string[] = ["4월 8일", "날씨 좋다"];
+  console.log(strArr);
+
+  //6. 튜플 변수 선언
+  const mixArr: [number, string] = [8, "날씨 좋다"];
+  console.log(mixArr);
+
+  //7. null 값 할당
+  const nothing: null = null;
+  console.log(nothing);
+
+  //8. undefined 값 할당
+  const undefinedValue: undefined = undefined;
+  console.log(undefinedValue);
+
+  //9. 객체 선언
+  const obj: object = {
+    xnmae: "꼬미",
+    age: 5,
+  };
+  console.log(obj);
+}
