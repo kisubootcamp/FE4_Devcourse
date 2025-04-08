bc4eb4222fe96e419bca72b4696b7e4b77f0b918
sduddla <sduddla921@gmail.com>
2025-04-08 15:48:00 +0900

    기본 타입 연습문제

---
+++ learn-ts/practice/02-primitive-types.ts
@@ -0,0 +1,58 @@
+{
+  // 1. 문자열 변수 선언
+  let h: string = 'Hello';
+  console.log(h);
+}
+
+{
+  // 2. 숫자 변수 선언
+  let num: number = 42;
+  console.log(num);
+}
+
+{
+  // 3. 불리언 변수 선언
+  let bool: boolean = true;
+  console.log(bool);
+}
+
+{
+  // 4. 숫자 배열 선언
+  let numArr: number[] = [1, 2, 3];
+  console.log(numArr);
+}
+
+{
+  // 5. 문자열 배열 선언
+  let stringArr: Array<string> = ['A', 'B', 'C'];
+  console.log(stringArr);
+}
+
+{
+  // 6. 튜블 변수 선언
+  let mixTuple: [number, string] = [2, 'hi'];
+}
+
+{
+  // 7. null값 할당
+  let n: null = null;
+  console.log(n);
+}
+
+{
+  // 8. undefined 값 할당
+  let un: undefined = undefined;
+  console.log(un);
+}
+
+{
+  // 9. 객체 선언
+  let mine: {
+    name: string;
+    age: number;
+  } = {
+    name: '박서영',
+    age: 25,
+  };
+  console.log(mine);
+}
