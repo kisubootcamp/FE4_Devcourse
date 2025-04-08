0e0fc53df3a300d920e823b82e5f1bc083a9b4f8
Suji Hwang <soojiya2003@gmail.com>
2025-04-08 12:49:35 +0900

    js 생성

---
+++ learn-js/과제/JS01.js
@@ -0,0 +1,20 @@
+// 1. 변수 선언과 할당
+let x = 10;
+let y = 20;
+let sum = x + y;
+console.log(sum);
+
+//2. 변수 재할당
+let xname = "Alice";
+xname = "Bob";
+console.log(xname);
+
+//3. 상수
+const PI = 3.14;
+console.log(PI);
+
+//4.변수 스왑
+let a = 5;
+let b = 10;
+a = b;
+b = a;
