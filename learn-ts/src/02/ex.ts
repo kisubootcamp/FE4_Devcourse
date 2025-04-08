12602b74fe76bb4b9ec9629cb303994c78356cd2
TYss00 <rlaxodus203@naver.com>
2025-04-08 12:49:22 +0900

    typetest

---
+++ learn-ts/src/02/ex.ts
@@ -0,0 +1,37 @@
+{
+    // 1문제 문자열 변수 선언
+    let one: string = 'Hello';
+    console.log(one);
+
+    // 2문제 숫자 변수 선언
+    let two: number = 42;
+    console.log(two);
+
+    // 3문제 불리언 변수 선언
+    let three: boolean = true;
+    console.log(three);
+
+    // 4문제 숫자 배열 선언
+    let four: number[] = [1, 2];
+    console.log(four);
+
+    // 5문제 문자열 배열 선언
+    let five: string[] = ['apple'];
+    console.log(five);
+
+    // 6문제 튜플 변수 선언
+    let six: [string, number] = ['apple', 1];
+    console.log(six);
+
+    // 7문제 null값 할당
+    let seven: null = null;
+    console.log(seven);
+
+    // 8문제 undefined값 할당
+    let eight: undefined = undefined;
+    console.log(eight);
+
+    // 9문제 객체선언
+    let nine: { name: string; age: number } = { name: 'TY', age: 26 };
+    console.log(nine);
+}
