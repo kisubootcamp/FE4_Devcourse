4e0d29afde13c3307d17d43b84913812c84a2ff9
Kyoeun Lee <lke159357@hufs.ac.kr>
2025-04-08 16:49:37 +0900

    ts 연습문제

---
+++ learn-ts/src/practice/기본 타입/Lv0.ts
@@ -0,0 +1,44 @@
+//연습문제
+{
+  // 1
+  let value: string = "Hello";
+  console.log(value);
+
+  // 2
+  let num: number = 42;
+  console.log(num);
+
+  // 3
+  let bool: boolean = true;
+  console.log(bool);
+
+  // 4
+  let numArr: number[] = [10, 20, 30];
+  console.log(numArr);
+
+  // 5
+  let stringArr: string[] = ["A", "B", "c"];
+  console.log(stringArr);
+
+  // 6
+  let mixArr: [number, string] = [10, "S"];
+  console.log(mixArr);
+
+  // 7
+  let nothing: null = null;
+  console.log(nothing);
+
+  // 8
+  let notDefined: undefined = undefined;
+  console.log(notDefined);
+
+  // 9
+  let user: {
+    name: string;
+    age: number;
+  } = {
+    name: "kyo",
+    age: 25,
+  };
+  console.log(user); // { name: 'kyo', age: 25 }
+}
