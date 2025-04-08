c794243c1e1f3d40ba5753fee2bb80cc0ee5c076
JiYoon Lee <leeyoon97@naver.com>
2025-04-08 17:54:01 +0900

    JS과제 조건문

---
+++ learn-js/S1/04/solution05.js
@@ -0,0 +1,11 @@
+const month = 7;
+
+if (month === 12 || month === 1 || month === 2) {
+  console.log("Winter");
+} else if (month === 3 || month === 4 || month === 5) {
+  console.log("Spring");
+} else if (month === 6 || month === 7 || month === 8) {
+  console.log("Summer");
+} else {
+  console.log("fall");
+}
