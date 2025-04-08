a7f605da75d4704714e1bfb780a014655e5cdedc
JiYoon Lee <leeyoon97@naver.com>
2025-04-09 01:23:21 +0900

    JS 연습문제 객체 4번까지

--- learn-js/S1/09/solution03.js
+++ learn-js/S1/09/solution03.js
@@ -1,20 +1,39 @@
 const obj = { a: 1, b: 5, c: 3 };
 const threshold = 2;
+const filterByThreshold = {};
 for (let key in obj) {
   if (obj[key] >= threshold) {
-    filterByThreshold.push(key, obj[key]);
+    filterByThreshold[key] = obj[key];
   }
 }
-console.log(filterByThreshold); // { b: 5, c: 3 }
+console.log(filterByThreshold);
 
 const obj2 = { x: 1, y: 2, z: 3 };
 const threshold2 = 2;
-console.log(filterByThreshold(obj2, threshold2)); // { y: 2, z: 3 }
+const filterByThreshold2 = {};
+for (let key in obj2) {
+  if (obj2[key] >= threshold2) {
+    filterByThreshold2[key] = obj2[key];
+  }
+}
+console.log(filterByThreshold2);
 
 const obj3 = { a: 10, b: 5, c: 1 };
 const threshold3 = 6;
-console.log(filterByThreshold(obj3, threshold3)); // { a: 10 }
+const filterByThreshold3 = {};
+for (let key in obj3) {
+  if (obj3[key] >= threshold3) {
+    filterByThreshold3[key] = obj3[key];
+  }
+}
+console.log(filterByThreshold3);
 
 const obj4 = { m: -1, n: 0, o: 1 };
 const threshold4 = 0;
-console.log(filterByThreshold(obj4, threshold4)); // { n: 0, o: 1 }
+const filterByThreshold4 = {};
+for (let key in obj4) {
+  if (obj4[key] >= threshold4) {
+    filterByThreshold4[key] = obj4[key];
+  }
+}
+console.log(filterByThreshold4);
