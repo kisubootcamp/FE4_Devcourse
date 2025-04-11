86db4f863a3a7b213b8f9c3a5770cb26063a0913
kisudevel <kiusevel@gmail.com>
2025-04-11 19:07:19 +0900

    typscript end

---
+++ learn-ts/src/01/index.ts
@@ -0,0 +1,7 @@
+{
+  // 코드 블럭 (중괄호)
+  let num: number = 10;
+
+  let numArr: [string, number] = ["A", 10];
+  numArr.push("A");
+}
---
+++ learn-ts/src/03/1.ts
@@ -0,0 +1,30 @@
+{
+  // 함수
+  // 매개 변수와 반환 값의 타입을 지정해주는 것
+  // return
+  // void -> 아무것도 반환하지 않는다
+  // 함수 선언문
+  // 함수 표현식
+  // 화살표 함수
+  // -> 함수 자체에 매개 변수와 반환 값의 타입을 지정하는 방법
+  // -> 변수에 함수의 매개 변수와 반환 값의 타입을 지정하는 방법
+  let num: number = 10;
+
+  const greet: () => void = () => {
+    console.log("Greet!");
+  };
+
+  const sum: (n1: number, n2: number) => number = (n1, n2) => n1 + n2;
+  const sun = (n1: number, n2: number): number => n1 + n2;
+
+  const printValue: (value: string) => void = (value) => {
+    console.log(value);
+  };
+
+  const add: (...numbers: number[]) => number = (...numbers) =>
+    numbers.reduce((result, num) => result + num, 0);
+
+  const printObj: (userObj: { name: string }) => void = (userObj) => {
+    console.log(userObj.name);
+  };
+}
