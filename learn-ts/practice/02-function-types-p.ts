d5d46a8267abcf2db6cdc4a21e1020bb35f3fc2e
sduddla <sduddla921@gmail.com>
2025-04-12 18:53:31 +0900

    함수 타입 연습문제 풀기

--- learn-ts/practice/02-function-types-p.ts
+++ learn-ts/practice/02-function-types-p.ts
@@ -1,28 +1,183 @@
 {
   // 1. 여러 개의 숫자 배열을 받아 합을 구하는 함수
-  const sumAllNumbers = (...arr: number[][]) => {
-    return arr.map((a) => a.reduce((x, y) => x + y));
+  const sumAllNumbers = (...num: number[][]): number[] => {
+    return num.map((a) => a.reduce((x, y) => x + y, 0));
   };
 
-  const result1 = sumAllNumbers([1, 2], [3, 4, 5], [6, 7]);
-  console.log(result1);
+  const result = sumAllNumbers([1, 2], [3, 4, 5], [6, 7]);
+  console.log(result); // [3, 12, 13]
 }
 
 {
-  // 2. 숫자 또는 문자열을 받아 그 값을 두 배로 만드는 함수
-  const doubleValue = (a: number | string) => {
+  // 2. 숫자 또는 문자열을 받아 그 값을 두배로 만드는 함수
+  const doubleValue = (a: number | string): number => {
     if (typeof a === 'number') {
       return a * 2;
     } else {
       return a.length * 2;
     }
   };
-  const result2 = doubleValue(5);
-  const result22 = doubleValue('hello');
-  console.log(result2);
-  console.log(result22);
+
+  const result1 = doubleValue(5);
+  const result2 = doubleValue('hello');
+  console.log(result1); // 10
+  console.log(result2); // 10
 }
 
 {
   // 3. 두 숫자 배열을 받아 각 배열의 곱을 구하는 함수
+
+  // 3-1.
+  const multiplyArrays = (a: number[], b: number[]): number[] => {
+    const arrLen = Math.min(a.length, b.length);
+    const result: number[] = [];
+
+    for (let i = 0; i < arrLen; i++) {
+      result.push(a[i] * b[i]);
+    }
+
+    return result;
+  };
+
+  const result = multiplyArrays([1, 2, 3], [4, 5, 6]);
+  console.log(result); // [ 4, 10, 18 ]
+
+  const result2 = multiplyArrays([1, 2], [4, 5, 6]);
+  console.log(result2); // [ 4, 10 ]
+
+  // 3-2.
+  const multiplyArrays2 = (a: number[], b: number[]): number[] => {
+    const minLength = Math.min(a.length, b.length);
+
+    return Array.from({ length: minLength }, (_, index) => a[index] * b[index]);
+  };
+  const result3 = multiplyArrays2([1, 2], [4, 5, 6]);
+  console.log(result3); // [4, 10]
+}
+
+{
+  // 4. 두 개의 배열을 받아 교차된 값을 반환하는 함수
+
+  // 4-1.
+  const intersection = (a: number[], b: number[]): number[] => {
+    const result: number[] = [];
+
+    for (let i = 0; i < a.length; i++) {
+      if (b.includes(a[i])) {
+        result.push(a[i] * a[i]);
+      }
+    }
+    return result;
+  };
+
+  const result = intersection([1, 2, 3], [2, 3, 4]);
+  console.log(result); // [4, 9]
+
+  // 4-2.
+  const intersection2 = (a: number[], b: number[]): number[] => {
+    return a.filter((value) => b.includes(value)).map((value) => value * value);
+  };
+
+  const result2 = intersection2([1, 2, 3], [2, 3, 4]);
+  console.log(result2); // [4, 9]
+}
+
+{
+  // 5. 객체의 필드 값에 따라 출력 형식을 다르게 하는 함수
+
+  // 5-1.
+  const formatInfo = (value: { name: string; age?: number }): void => {
+    if (value.name) {
+      console.log(`Name: ${value.name}`); // Name: Alice
+    }
+    if (value.age) {
+      console.log(`Age: ${value.age}`);
+    }
+  };
+
+  const info = formatInfo({ name: 'Alice' });
+
+  // 5-2.
+  const formatInfo2 = (value: { name: string; age?: number }): string => {
+    if (value.name) {
+      return `Name: ${value.name}`;
+    }
+    if (value.age) {
+      return `Age: ${value.age}`;
+    }
+    throw new Error('Type Invalid');
+  };
+
+  const info2 = formatInfo2({ name: 'Alice' });
+  console.log(info2); // Name: Alice
+}
+
+{
+  // 6. 두 문자열을 합쳐서 출력하는 함수
+  const combineStrings = (a: string, b: string): string => {
+    return `${a}${b}`;
+  };
+
+  const combined = combineStrings('Hello', 'World');
+  console.log(combined); // HelloWorld
+}
+
+{
+  // 7. 배열의 각 값을 제곱하여 새로운 배열을 반환하는 함수
+  const squareArray = (num: number[]): number[] => {
+    return num.map((a) => a * a);
+  };
+
+  const result = squareArray([1, 2, 3, 4]);
+  console.log(result); // [ 1, 4, 9, 16 ]
+}
+
+{
+  // 8. 문자열과 숫자가 주어졌을 때, 그 둘을 더하여 문자열로 반환하는 함수
+  const addStringAndNumber = (a: string, b: number): string => {
+    return `${a}${b}`;
+  };
+
+  const result = addStringAndNumber('The number is ', 10);
+  console.log(result); // The number is 10
+}
+
+{
+  // 9. 타입이 서로 다른 두 값을 비교하는 함수
+
+  // 9-1.
+  const compareValues = (a: number, b: string): string => {
+    if (a > b.length) {
+      return 'Number is larger';
+    } else {
+      return 'String is larger';
+    }
+  };
+
+  const result = compareValues(10, 'Hello');
+  console.log(result); // Number is larger
+
+  // 9-2.
+  const compareValues2 = (a: number, b: string): string => {
+    return a > b.length ? 'Number is larger' : 'String is larger';
+  };
+
+  const result2 = compareValues2(10, 'Hello');
+  console.log(result2); // Number is larger
+}
+
+{
+  // 10. 다중 조건을 처리하는 조건문 함수
+  const checkValue = (a: number | string | boolean): string => {
+    if (typeof a === 'number') {
+      return "It's a number";
+    } else if (typeof a === 'string') {
+      return "It's a string";
+    } else {
+      return "It's a boolean";
+    }
+  };
+
+  const result = checkValue(true);
+  console.log(result); // It's a boolean
 }
--- learn-ts/practice/02-function-types.ts
+++ learn-ts/practice/02-function-types.ts
@@ -1,118 +1,169 @@
 {
   // 1. 두 수의 합을 구하는 함수
-  const add = function add(n1: number, n2: number): number {
-    return n1 + n2;
-  };
-  const result = add(10, 5);
-  console.log(result);
 
-  // const add1 = (n1: number, n2: number): number => n1 + n2;
-  // const result1 = add1(10, 5);
-  // console.log(result1);
+  // 1-1.
+  const add = (a: number, b: number): number => {
+    return a + b;
+  };
+
+  const result = add(10, 5);
+  console.log(result); // 15
+
+  // 1-2.
+  const add2 = function add2(a: number, b: number): number {
+    return a + b;
+  };
+
+  const result2 = add2(10, 5);
+  console.log(result2); // 15
 }
 
 {
   // 2. 이름을 받아 인사하는 함수
-  const greet = function greet(name: string): string {
-    return `Hello, ${name}!`;
-  };
-  const greeting = greet('Alice');
-  console.log(greeting);
 
-  // const greet2 = (name: string): void => console.log(`Hello, ${name}!`);
-  // const greeting2 = greet2('Alice');
+  // 2-1.
+  const greet = (name: string): string => {
+    return `Hello, ${name}`;
+  };
+
+  const greeting = greet('Alice');
+  console.log(greeting); // Hello, Alice
+
+  // 2-2.
+  const greet2 = (name: string): void => console.log(`Hello, ${name}`); // Hello, Alice
+  const greeting2 = greet2('Alice');
 }
 
 {
   // 3. 숫자 배열을 받아 합을 구하는 함수
-  const sumAll = function sumAll(...num: number[]): number {
+  const sumAll = (...num: number[]): number => {
     return num.reduce((a, b) => a + b, 0);
   };
-  const total = sumAll(1, 2, 3, 4);
-  console.log(total);
 
-  // const sumAll3 = (...num: number[]): number => num.reduce((a, b) => a + b, 0);
-  // const total3 = sumAll3(1, 2, 3, 4);
-  // console.log(total3);
+  const total = sumAll(1, 2, 3, 4);
+  console.log(total); // 10
 }
 
 {
   // 4. 기본값을 설정한 덧셈 함수
-  const sum = function sum(num: number, num1: number = 0): number {
-    return num + num1;
-  };
-  const result = sum(5);
-  console.log(result);
 
-  // const sum4 = (a: number, b: number = 0): number => a + b;
-  // const result4 = sum4(5);
-  // console.log(result4);
+  // 4-1.
+  const sum = (a: number, b: number = 0): number => {
+    return a + b;
+  };
+
+  const result = sum(5);
+  console.log(result); // 5
+
+  // 4-2.
+  const sum2 = (a: number, b: number = 0): void => console.log(a + b);
+  const result2 = sum2(5);
 }
 
 {
   // 5. 두 수를 곱하는 함수
-  const multiply: (a: number, b: number) => number = (a, b) => {
+  const multiply = (a: number, b: number): number => {
     return a * b;
   };
-  const product = multiply(4, 5);
-  console.log(product);
 
-  // const multiply5 = (a: number, b: number) => a * b;
-  // const product5 = multiply5(4, 5);
-  // console.log(product5);
+  const product = multiply(4, 5);
+  console.log(product); // 20
 }
 
 {
   // 6. 문자열과 숫자를 결합하는 함수
-  const concatStringAndNumber = function concatStringAndNumber(
-    a: string,
-    b: number
-  ) {
-    return a + b;
-  };
-  const result = concatStringAndNumber('Hello', 10);
-  console.log(result);
 
-  // const concatStringAndNumber6 = (a: string, b: number): string => `${a}${b}`;
-  // const result6 = concatStringAndNumber6('Hello', 10);
-  // console.log(result6);
+  // 6-1.
+  const concatStringAndNumber = (a: string, b: number): string => {
+    return `${a}${b}`;
+  };
+
+  const result = concatStringAndNumber('Hello', 10);
+  console.log(result); // Hello10
+
+  // 6-2.
+  const concatStringAndNumber2: (a: string, b: number) => string = (a, b) => {
+    return `${a}${b}`;
+  };
+  const result2 = concatStringAndNumber2('Hello', 10);
+  console.log(result2); // Hello10
 }
 
 {
   // 7. 옵셔널 파라미터를 사용한 인사 함수
-  const greet = (name: string, message: string = 'Welcome') => {
-    console.log(`${message}, ${name}`);
+
+  // 7-1.
+  const greet = (name: string, message?: string): string => {
+    if (!message) return `Welcome, ${name}!`;
+    else return `${message}, ${name}!`;
   };
+
   const greeting1 = greet('Alice', 'Hello');
   const greeting2 = greet('Bob');
+  console.log(greeting1); // Hello, Alice!
+  console.log(greeting2); // Welcome, Bob!
+
+  // 7-2.
+  const greet2: (name: string, message?: string) => void = (
+    name: string,
+    message: string = 'Welcome'
+  ) => {
+    console.log(`${message}, ${name}!`);
+  };
+  const greeting3 = greet2('Alice', 'Hello'); // Hello, Alice!
+  const greeting4 = greet2('Bob'); // // Welcome, Bob!
 }
 
 {
   // 8. 숫자 배열의 최대값을 구하는 함수
-  const findMax = function findMax(num: number[]): number {
+
+  // 8-1.
+  const findmax = (num: number[]): number => {
+    const max = num.sort((a, b) => a - b);
+    return max[max.length - 1];
+  };
+
+  const max = findmax([10, 20, 30, 40]);
+  console.log(max); // 40
+
+  // 8-2.
+  const findmax2 = (num: number[]): number => {
+    return Math.max(...num);
+  };
+  const max2 = findmax2([10, 20, 30, 40]);
+  console.log(max2); // 40
+
+  // 8-3.
+  const findmax3 = (num: number[]): number => {
     let max = num[0];
+
     for (let i = 0; i < num.length; i++) {
-      if (max < num[i]) max = num[i];
+      if (max < num[i]) {
+        max = num[i];
+      }
     }
     return max;
   };
-  const max = findMax([10, 20, 30, 40]);
-  console.log(max);
-
-  // const findMax2 = (nums: number[]): number => Math.max(...nums);
-  // const max2 = findMax2([10, 20, 30, 40]);
-  // console.log(max2);
+  const max3 = findmax3([10, 20, 30, 40]);
+  console.log(max3); // 40
 }
 
 {
   // 9. 단일 숫자를 두 배로 만드는 함수
-  const double = function double(num: number) {
-    return num * 2;
-  };
-  const doubled = double(10);
-  console.log(doubled);
 
-  // const double9 = (num: number): number => num * 2;
-  // const doubled9 = double9(10);
-  // console.log(doubled9);
+  // 9-1.
+  const double = (a: number): number => {
+    return a * 2;
+  };
+
+  const doubled = double(10);
+  console.log(doubled); // 20
+
+  // 9-2.
+  const double2 = function double2(a: number): number {
+    return a * 2;
+  };
+
+  const doubled2 = double2(10);
+  console.log(doubled2); // 20
 }
