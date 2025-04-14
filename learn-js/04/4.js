1fdf38b52fe8ced43bcdcf34399cc60c5d48664e
sduddla <sduddla921@gmail.com>
2025-04-14 22:37:58 +0900

    02, 03, 04 js 수업

---
+++ learn-js/04/4.js
@@ -0,0 +1,11 @@
+// 표준 내장 객체
+
+const a = new String('Hello');
+console.log(a.toUpperCase());
+
+// string, number, boolean
+// 래퍼 객체
+const b = 'hello';
+console.log(b.toUpperCase());
+// console.log(new String(b).toUpperCase());
+// console.log(b.toUpperCase);
