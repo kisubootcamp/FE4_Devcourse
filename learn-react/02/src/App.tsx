f3f6cbb3ae4cee603485e366276fb8bf56f3ee35
dlakrp123 <dlakrp731@gmail.com>
2025-04-15 22:52:10 +0900

    react 정리2

--- learn-react/02/src/App.tsx
+++ learn-react/02/src/App.tsx
@@ -2,6 +2,11 @@ import './css/index.css'; //글로벌 스타일 css
 import styles from './components/App.module.css'; //module.css
 import classNames from 'classnames'; //조건부 클래스 설정
 import classNames2 from 'classnames/bind';
+import styled from 'styled-components'; //CSS-IN-JS 방법
+const Title = styled.h2`
+  color: purple;
+  font-size = 24px;
+`;
 export default function App() {
   const cx = classNames2.bind(styles); //bind 적용
   const isBold = true;
@@ -15,7 +20,9 @@ export default function App() {
       <h2 className={classNames(styles.title, { [styles.bold]: isBold })}>
         css module
       </h2>
-      <h2 className={cx('title', 'bold')}>css module2</h2>
+      <h2 className={cx('title', { bold: isBold })}>css module2</h2>
+      {/* CSS-IN-JSS */}
+      <Title>styled-components</Title>
     </>
   );
 }
