73ede785fc0a5c91838e848a7f0d6c6102c33918
TYss00 <rlaxodus203@naver.com>
2025-04-15 16:07:50 +0900

    415

---
+++ learn-react/02/Note.ts
@@ -0,0 +1,22 @@
+/*
+1. 리액트 스타일링 방법
+
+1.1 전통적인 방법으로 스타일링하기
+1.1.1 인라인 스타일
+1.1.2 글로벌(외부) 스타일
+1.1.3 CSS 모듈 (+ classnames 패키지)
+//https://www.npmjs.com/package/classnames 여기서 패키지 설치하고 사용함
+
+1.2 CSS-IN-JS 방법으로 스타일링하기
+https://styled-components.com/docs/basics#installation
+1.2.1 styled-components
+1.2.2 emotion
+1.2.3 Vanilia Extract
+
+1.3 TailwindCSS 방법으로 스타일링하기
+1.3.1 단독 유틸리티 퍼스트 CSS 라이브러리
+
+1.4 그외
+1.4.1 이미지 랜더링하기
+1.4.2 폰트 적용하기
+*/
