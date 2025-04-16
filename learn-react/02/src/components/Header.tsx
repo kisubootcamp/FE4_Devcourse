ef84343c08802913eda49024fe8f9996d1daf212
kyoeun <lke159357@hufs.ac.kr>
2025-04-16 09:03:58 +0900

    리액트  정리

---
+++ learn-react/02/src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  const style = { color: "red", textDecoration: "underline" };
+  return (
+    <>
+      <header>
+        <h1 style={style}>내 웹사이트</h1>
+      </header>
+    </>
+  );
+}
+// 인라인 스타일 방법
