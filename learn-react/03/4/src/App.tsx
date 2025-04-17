1c401fe52078b190ee8089c4b08ef146aee32f25
hyeju <hailey_apple@icloud.com>
2025-04-17 17:34:10 +0900

    [FE5_현혜주] 20250417_01_과제제출

--- learn-react/03/4/src/App.tsx
+++ learn-react/03/4/src/App.tsx
@@ -3,7 +3,7 @@ import SignUp from "./components/SignUp";
 
 export default function App() {
   return (
-    <div className="wrapper bg-black py-10 w-[100vw] h-[100%] flex flex-col justify-center items-center gap-2">
+    <div className="wrapper bg-black py-10 w-[100vw] h-[100vh] flex justify-center items-center gap-2">
       <Login />
       <SignUp />
     </div>
