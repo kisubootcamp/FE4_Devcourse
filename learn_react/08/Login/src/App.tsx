8a1d2d0ba36ae77eb571ef5c353a5d03ee0237ab
JINJIYU23 <dhkswksla22@naver.com>
2025-04-17 17:27:54 +0900

    로그인 및 회원가입 TailwindCss

---
+++ learn_react/08/Login/src/App.tsx
@@ -0,0 +1,11 @@
+// import { twMerge } from "tailwind-merge";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+export default function App() {
+  return (
+    <>
+      <Login />
+      <SignUp />
+    </>
+  );
+}
