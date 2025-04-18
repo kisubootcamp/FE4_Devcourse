fb7ed3caf4159e10a18f6cfd2013024612c62462
mjlee <leeminji9938@naver.com>
2025-04-18 11:24:51 +0900

    폴더 정리

--- learn-react/05/1/src/App.tsx
+++ learn-react/05/1/src/App.tsx
@@ -1,7 +1,29 @@
+import User from "./components/User";
+
 export default function App() {
+  const user = {
+    id: 1,
+    name: "sucoding",
+    age: 20,
+  };
   return (
     <>
-      <h1 className='text-3xl underline'>App Component</h1>
+      {/* 
+        1. 객체를 객체 그대로 던진다
+         <User user={user} /> 
+      */}
+      {/* 
+        2. 
+          <User id={user.id} name={user.name} age={user.age} /> 
+      */}
+      {/* 
+        3. User 객체의 요소를 전개해서 props에 전달 -> 객체에만 사용 가능 -> 왜? 객체는 속성과 값이 명확해서 
+          내부적으로 ⬇️ 이렇게 전달하는거랑 똑같음
+          <User id={user.id} name={user.name} age={user.age} /> 
+      */}
+
+      {/* 전개 연산자 사용 */}
+      <User {...user} clickHandler={() => alert("click")} />
     </>
   );
 }
---
+++ learn-react/05/2/main.js
@@ -0,0 +1,3 @@
+const numArr = [1, 2, 3, 4]; // 2배 한 배열
+const numArr2 = numArr.map((num) => num * 2); // 각각의 요소만큼 순회하면서 어떠한 값을 반환함 (새로운 배열로)
+console.log(numArr2);
