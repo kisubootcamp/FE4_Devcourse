2391e0b76da7b41ec5ea82acc508c662d2203aad
mjlee <leeminji9938@naver.com>
2025-04-21 14:22:00 +0900

    useState 연습문제

---
+++ learn-react/06/ColorBox/src/App.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+export default function ColorBox() {
+  const [input, setInput] = useState("");
+  const chgangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  return (
+    <>
+      <div className='item-middle'>
+        <div className='flex flex-col'>
+          <div className='w-40 h-40 border border-slate-500' style={{backgroundColor: input}}></div>
+          <input
+            type='text'
+            className='border border-slate-500 w-40 mt-2'
+            value={input}
+            onChange={chgangeHandler}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
