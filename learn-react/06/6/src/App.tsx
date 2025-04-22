57feeb5e9f4541f112078bbf4b16c3ee14800943
kisudevel <kiusevel@gmail.com>
2025-04-22 17:15:14 +0900

    07일차

--- learn-react/06/6/src/App.tsx
+++ learn-react/06/6/src/App.tsx
@@ -1,11 +1,11 @@
 export default function App() {
   const picture = [
-    "https://cdn.pixabay.com/photo/2013/08/26/09/40/silhouette-175970_1280.jpg",
-    "https://cdn.pixabay.com/photo/2015/11/25/09/42/rocks-1061540_1280.jpg",
-    "https://cdn.pixabay.com/photo/2018/09/23/12/33/building-3697342_1280.jpg",
-    "https://cdn.pixabay.com/photo/2014/05/02/12/43/clouds-335969_1280.jpg",
-    "https://cdn.pixabay.com/photo/2022/12/28/21/10/streets-7683842_1280.jpg",
-    "https://cdn.pixabay.com/photo/2023/01/08/05/45/mountain-7704584_1280.jpg",
+    "https://dummyimage.com/600x400/000/fff&text=0",
+    "https://dummyimage.com/600x400/000/fff&text=1",
+    "https://dummyimage.com/600x400/000/fff&text=2",
+    "https://dummyimage.com/600x400/000/fff&text=3",
+    "https://dummyimage.com/600x400/000/fff&text=4",
+    "https://dummyimage.com/600x400/000/fff&text=5",
   ];
 
   return (
