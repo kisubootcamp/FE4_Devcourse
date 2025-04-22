f97b6cefdf26394bda42ce3bebd655640bfc543b
sduddla <sduddla921@gmail.com>
2025-04-23 02:21:04 +0900

    여행사이트

--- learn-react/06/7/src/App.tsx
+++ learn-react/06/7/src/App.tsx
@@ -4,45 +4,45 @@ import london from './assets/images/london.jpg';
 import paris from './assets/images/paris.jpg';
 import newyork from './assets/images/newyork.jpg';
 import { useState } from 'react';
-export default function App() {
-  const [city, setCity] = useState('Seoul');
+import { twJoin } from 'tailwind-merge';
 
+export default function App() {
+  const areas = ['Seoul', 'London', 'Paris', 'NewYork'];
+  const images: { [key: string]: string } = {
+    travel,
+    seoul,
+    london,
+    paris,
+    newyork,
+  };
+  const [location, setLoaction] = useState('Seoul');
   return (
     <>
+      {/* 다크모드 */}
       <div className="absolute top-10 left-10 text-3xl cursor-pointer">
         <div className="light-icon fa-regular fa-sun"></div>
-        <div className="dark-icon fa-regular fa-moon hidden"></div>
+        <div className="light-icon fa-regular fa-moon"></div>
       </div>
       <div className="flex justify-center items-center min-h-screen">
         <div className="flex flex-col items-center gap-4  max-w-[500px]">
           <img src={travel} alt={'로고'} width={80} />
           <ul className="flex items-center antialiased justify-around w-full">
-            <li>
-              <a href="#" onClick={() => setCity('Seoul')}>
-                Seoul
-              </a>
-            </li>
-            <li>
-              <a href="#" onClick={() => setCity('London')}>
-                London
-              </a>
-            </li>
-            <li>
-              <a href="#" onClick={() => setCity('Paris')}>
-                Paris
-              </a>
-            </li>
-            <li>
-              <a href="#" onClick={() => setCity('NewYork')}>
-                NewYork
-              </a>
-            </li>
+            {areas.map((area) => (
+              <li className={twJoin(location === area && 'font-bold')}>
+                <a href="#" onClick={() => setLoaction(area)}>
+                  {area}
+                </a>
+              </li>
+            ))}
           </ul>
           <div>
-            {city === 'Seoul' && <img src={seoul} alt={'서울'} />}
-            {city === 'London' && <img src={london} alt={'런던'} />}
-            {city === 'Paris' && <img src={paris} alt={'파리'} />}
-            {city === 'NewYork' && <img src={newyork} alt={'뉴욕'} />}
+            {areas.map((area) => {
+              return (
+                location === area && (
+                  <img src={images[area.toLocaleLowerCase()]} alt={area} />
+                )
+              );
+            })}
           </div>
         </div>
       </div>
