a1c225e37a9957da0452cc75b496faf692158a04
Bomin Kim <sirubom1203@gmail.com>
2025-04-23 23:26:15 +0900

    과제제출

---
+++ learn-react/state_master/src/redux/slice/counterSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counterSlice",
+  initialState: {
+    value: 0,
+  },
+  // 상태값을 업데이트 할 수 있는 로직
+  reducers: {
+    increment(state) {
+      state.value += 1;
+    },
+    decrement(state) {
+      state.value -= 1;
+    },
+    reset(state) {
+      state.value = 0;
+    },
+  },
+});
+
+export const { increment, decrement, reset } = counterSlice.actions;
+export default counterSlice.reducer;
