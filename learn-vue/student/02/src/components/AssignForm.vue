6f5c159a12569132ce3d012419b1eb7c1cebc11e
Suji Hwang <soojiya2003@gmail.com>
2025-05-21 12:54:01 +0900

    vue02 연습문제

---
+++ learn-vue/student/02/src/components/AssignForm.vue
@@ -0,0 +1,71 @@
+<script>
+export default {
+  name: 'AssignForm',
+  data() {
+    return {
+      name: '',
+      isChecked: 'false',
+      age: 25,
+      text: '',
+      fruit: 'apple',
+      fruits: [],
+      gender: 'male',
+      color: '#ffffff',
+    }
+  },
+  methods: {
+    changeHandler(e) {
+      this.name = e.target.value
+    },
+    textChange(e) {
+      this.text = e.target.value
+    },
+  },
+}
+</script>
+<template>
+  <p>문제 1</p>
+  <h1>{{ name }}</h1>
+  <input type="text" @input="changeHandler" />
+
+  <p>문제 2</p>
+  <h1>{{ isChecked ? '동의' : '비동의' }}</h1>
+  <input type="checkbox" v-model="isChecked" />
+  약관에 동의힙니다.
+
+  <p>문제 3</p>
+  <h1>{{ age }}</h1>
+  <input type="range" v-model="age" min="1" max="100" />
+
+  <p>문제 4</p>
+  <h1>{{ text }}</h1>
+  <input type="textarea" @input="textChange" />
+
+  <p>문제 5</p>
+  <h1>{{ fruit }}</h1>
+  <select v-model="fruit">
+    <option value="apple">apple</option>
+    <option value="banana">banana</option>
+    <option value="orange">orange</option>
+  </select>
+
+  <p>문제 6</p>
+  <div>
+    <label> <input type="checkbox" v-model="fruits" value="apple" />apple </label>
+    <label> <input type="checkbox" v-model="fruits" value="banana" />banana </label>
+    <label> <input type="checkbox" v-model="fruits" value="orange" />orange </label>
+    <h1>{{ fruits }}</h1>
+  </div>
+  <p>문제 7</p>
+  <h1>{{ gender }}</h1>
+  <input type="radio" v-model="gender" value="male" />male
+  <input type="radio" v-model="gender" value="female" />female
+
+  <p>문제 8</p>
+  <h1>{{ color }}</h1>
+  <div
+    :style="{ width: '100px', height: '100px', border: '1px solid black', backgroundColor: color }"
+  ></div>
+  <input type="color" v-model="color" />
+</template>
+<style scoped></style>
---
+++ learn-vue/student/02/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+import Assign from './components/Assign.vue'
+
+createApp(Assign).mount('#app')
