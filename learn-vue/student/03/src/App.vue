196ef6c2e909361c33ea517dcd7d84eb444908e6
yubin121 <yubin045@naver.com>
2025-05-22 19:11:28 +0900

    Vue 3일차 수업

---
+++ learn-vue/student/03/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import CountGroup from './components/CountGroup.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CountGroup,
+  },
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  provide() {
+    return {
+      count: () => this.count,
+      increment: this.increment,
+      reset: this.reset,
+      decrement: this.decrement,
+      incrementByAmount: this.incrementByAmount,
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1
+    },
+    reset() {
+      this.count = 0
+    },
+    decrement() {
+      this.count -= 1
+    },
+    incrementByAmount(amount) {
+      this.count += amount
+    },
+  },
+}
+</script>
+<template>
+  <!-- provide 방식 -->
+  <CountGroup />
+</template>
+<style scoped></style>
