85cc374a23545dd19ffcb488eb0bd14459d95108
Hayoung Kang <rkdgkdudsam@naver.com>
2025-05-22 16:37:37 +0900

    뷰 03

---
+++ learn-vue/03/src/components/CountDisplay.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'CountDisplay',
+  props: {
+    count: Number,
+  },
+}
+</script>
+<template>
+  <h1>Count: {{ count }}</h1>
+</template>
+<style scoped></style>
