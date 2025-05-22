ffea6257913fd611aa012bb44cd8ca7d711860b7
ygvbhy <ygvbhy@naver.com>
2025-05-22 21:05:23 +0900

    4일차 정리

---
+++ learn-vue/student/03/src/components/CountButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="reset">reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ['increment', 'decrement', 'reset'],
+}
+</script>
+
+<style lang="scss" scoped></style>
