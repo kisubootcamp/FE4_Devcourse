4fcaa2246d74e585705158f0464733050fb03b12
ty <composedkim@naver.com>
2025-05-25 16:36:01 +0900

    투두 리스트 과제제출

---
+++ learn-vue/student/todo_list/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<script>
+import TodoListItem from './TodoListItem.vue';
+
+export default {
+  components: {
+    TodoListItem,
+  },
+  data() {
+    return {};
+  },
+  props: {
+    todos: {
+      type: Array,
+    },
+  },
+  methods: {
+    doneTodo(id) {
+      this.$emit('done-todo', id);
+    },
+    deleteTodo(id) {
+      this.$emit('delete-todo', id);
+    },
+  },
+};
+</script>
+<template>
+  <ul class="divide-y divide-gray-200">
+    <TodoListItem v-for="todo in todos" :key="todo.id" :todo="todo" @done-todo="doneTodo" @delete-todo="deleteTodo" />
+  </ul>
+</template>
+<style></style>
