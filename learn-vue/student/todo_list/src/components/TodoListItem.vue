343b443f20ae4a0fc8408b71c703b877f005254a
chosojeong97 <chosojeong97@gmail.com>
2025-05-25 16:11:16 +0900

    과제

---
+++ learn-vue/student/todo_list/src/components/TodoListItem.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    todo: Object,
+  },
+  emits: ['deleteTodo', 'checkedTodo'],
+};
+</script>
+<template>
+  <li class="flex items-center justify-between p-3 border-b hover:bg-gray-100 transition-colors">
+    <div :class="['flex items-center group', todo.checked && 'checked']">
+      <input
+        type="checkbox"
+        :class="`mr-3 h-4 w-4 text-blue-500 focus:ring-blue-400`"
+        :checked="this.todo.checked"
+        @click="$emit('checkedTodo', todo.id)"
+      />
+      <span class="group-[.checked]:text-gray-400 group-[.checked]:line-through">{{ this.todo.title }}</span>
+    </div>
+    <button @click="$emit('deleteTodo', todo.id)" class="text-red-500 hover:text-red-700 ml-4">Delete</button>
+  </li>
+</template>
