c6907bbc63da501cc4fe3eca30c457209f82c61d
JINJIY23 <dhkswksla22@naver.com>
2025-05-25 16:20:25 +0900

    todolist

---
+++ learn_vue/student/todo_list/src/components/Todo.vue
@@ -0,0 +1,42 @@
+<script>
+import TodoEditor from "./TodoEditor.vue";
+import TodoHeader from "./TodoHeader.vue";
+import TodoList from "./TodoList.vue";
+
+let id = 0;
+export default {
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  components: {
+    TodoHeader,
+    TodoEditor,
+    TodoList,
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo !== "") {
+        this.todos.push({ id: id++, text: this.newTodo, done: false });
+        this.newTodo = "";
+      }
+    },
+    delTodo(todo) {
+      this.todos = this.todos.filter((t) => t.id !== todo.id);
+    },
+    // toggleTodo(todo) {
+    //   todo.done = !todo.done;
+    // },
+  },
+};
+</script>
+<template>
+  <div class="max-w-md mx-auto shadow-lg rounded-lg overflow-hidden">
+    <TodoHeader />
+    <TodoEditor @add-todo="addTodo" v-model="newTodo" />
+    <TodoList @del-todo="delTodo" :todos="todos" />
+  </div>
+</template>
+<style></style>
---
+++ learn_vue/student/todo_list/src/components/TodoListItem.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    todo: {
+      type: Object,
+    },
+  },
+  emits: ["del-todo"],
+};
+</script>
+<template>
+  <li
+    class="flex items-center justify-between p-3 border-b hover:bg-gray-100 transition-colors"
+  >
+    <div class="flex items-center">
+      <input
+        v-model="todo.done"
+        type="checkbox"
+        :class="`mr-3 h-4 w-4 text-blue-500 focus:ring-blue-400`"
+      />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+    </div>
+    <button
+      @click="$emit('del-todo', todo)"
+      class="text-red-500 hover:text-red-700 ml-4"
+    >
+      Delete
+    </button>
+  </li>
+</template>
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
