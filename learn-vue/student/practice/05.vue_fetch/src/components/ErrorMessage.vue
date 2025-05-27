1720d23fa362bafbec94398361cfedb8ed7b82a7
hxezu <hailey_apple@icloud.com>
2025-05-27 15:37:46 +0900

    🎉 Fetch JSON 코드 추가

---
+++ learn-vue/student/practice/05.vue_fetch/src/components/ErrorMessage.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  message: String,
+  retry: Function,
+})
+</script>
+<template>
+  <h1>Error</h1>
+  <h2>{{ message }}</h2>
+  <button @click="retry">retry</button>
+</template>
+<style scoped></style>
--- learn-vue/student/practice/05.vue_fetch/src/router/index.js
+++ learn-vue/student/practice/05.vue_fetch/src/router/index.js
@@ -10,17 +10,17 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/post/:id(\\d+)',
+      name: 'post-detail',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/PostDetailView.vue'),
     },
     {
-      path: '/data-fetch',
-      name: 'dataFetch',
-      component: () => import('../views/DataView.vue'),
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('../views/NotFound.vue'),
     },
   ],
 })
