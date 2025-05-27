49b113e6a42d13a47d717fe0406c9718b561fc9f
hxezu <hailey_apple@icloud.com>
2025-05-27 11:27:45 +0900

    🎉 Pinia 강의 내용 추가

---
+++ learn-vue/student/practice/04.todo(pinia)/src/components/TodoList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useTodoStore } from "@/stores/todo";
+import TodoListItem from "./TodoListItem.vue";
+
+const todoStore = useTodoStore();
+</script>
+<template>
+  <ul class="divide-y divide-gray-200">
+    <TodoListItem />
+  </ul>
+</template>
+<style></style>
