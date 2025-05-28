1d69763071c11ade638763321e35f482b7c789cb
TYss00 <rlaxodus203@naver.com>
2025-05-28 21:18:34 +0900

    0528_movies

---
+++ learn-vue/07/movies/vue/src/views/DetailView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Detailinfo from '@/components/detail/DetailInfo.vue'
+import DetailTrailer from '@/components/detail/DetailTrailer.vue'
+import MainMovie from '@/components/main/MainMovie.vue'
+</script>
+<template>
+  <DetailTrailer />
+  <Detailinfo />
+  <MainMovie />
+</template>
+<style scoped></style>
