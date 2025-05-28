54550fe121108d17952704d379d71536805f81a1
Suji Hwang <soojiya2003@gmail.com>
2025-05-28 13:44:38 +0900

    Vue 05

---
+++ learn-vue/student/05/src/components/FirstChild.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({ name: String, age: { type: Number, required: true } })
+
+const emit = defineEmits('increment')
+</script>
+<template>
+  <h1>FirstChildvue</h1>
+  <h1>{{ props.name }}</h1>
+  <h1>{{ props.age }}</h1>
+  <button @click="emit('increment')">click</button>
+</template>
+<style scoped></style>
