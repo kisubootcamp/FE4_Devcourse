0b09bff05f0fdfe242be677ad2cefa0a9139db36
Jungyu Park <parkjungyu09@gmail.com>
2025-05-29 21:53:13 +0900

    🚀 Supabase: Github 로그인 구현

---
+++ learn-reactv2/react-router/src/routes/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then(setPosts);
+  }, []);
+  return (
+    <>
+      <h1>Home</h1>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </>
+  );
+}
